ba9baa3dae4df7c26d629228683018dfc8fb5108
Swistusmen <michal76aw@gmail.com>
2021-02-02 20:36:50 +0100

    Template google test repo

---
+++ src/Example.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<iostream>
+#include <string>
+
+
+std::string fun(){return "Hello";}
+
+int num() { return 5; }
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Example.h"
+
+int main()
+{
+    std::cout<<fun();
+    return 0;
+}
