a96d6e1608d3ace055a654178206f8f147e1189a
Swistusmen <michal76aw@gmail.com>
2021-04-26 23:22:21 +0200

    testing signals

--- src/QMainInterface.cpp
+++ src/QMainInterface.cpp
@@ -6,4 +6,12 @@ QMainInterface::QMainInterface(QWidget* parent) : QWidget(parent)
 	//setFixedSize(366, 300);
 
 	ui.setupUi(this);
+	ui.label->setParent(this);
+	ui.algoInterface->setParent(this);
+	connect(this, SIGNAL(algoWasClicked()),
+		this, SLOT(changeTitle(QString)));
+	connect(this, SIGNAL(this->dataWasClicked()),
+		this, SLOT(changeTitle(QString)));
+	//initializing main image
+
 }
--- src/QMainInterface.h
+++ src/QMainInterface.h
@@ -3,6 +3,7 @@
 
 #include <qwidget.h>
 #include "../build/src/ui_QMainInterface.h"
+#include <qstringbuilder.h>
 
 
 class QPushButton;
@@ -13,11 +14,22 @@ public:
 	explicit QMainInterface(QWidget* parent=0);
 
 signals:
-
+	void algoWasClicked() {
+		emit(test_1);
+	};
+	void dataWasClicked() {
+		emit(test_2);
+	}
 public slots:
-
+	void changeTitle(QString display) {
+		ui.label->setText(display);
+	}
 private:  //variables
 	Ui::Form ui;
+
+	QString test_1="Geometrical Algorithms";
+	QString test_2="Data";
+	QString to_display = "Geometrical Algorithms";
 
 private: //methods
 
